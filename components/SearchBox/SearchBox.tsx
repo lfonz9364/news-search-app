be8bb72897b79619d998f6b5d747f0c49591c2ac
Alfons Caroles <acaroles@whispir.com>
2025-01-27 10:56:10 +1100

    Added search box

---
+++ components/SearchBox/SearchBox.tsx
@@ -0,0 +1,61 @@
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, useColorScheme, View} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+import {SearchBar} from '@rneui/themed';
+
+import {SearchBoxProps} from './types';
+import axios from 'axios';
+
+const SearchBox = ({
+  onSearchResultReturned,
+}: SearchBoxProps): React.JSX.Element => {
+  const [keyword, setKeyword] = useState('');
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const searchNews = (search: string) => {
+    setKeyword(search);
+  };
+
+  const getNews = async () => {
+    try {
+      const responses = await axios.get(
+        'https://newsapi.org/v2/everything?q=Trump&apiKey=183daca270264bad86fc5b72972fb82a',
+      );
+      console.log(responses);
+      onSearchResultReturned(responses.data.articles);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    Promise.resolve(getNews());
+  });
+
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          backgroundColor: isDarkMode ? Colors.black : Colors.white,
+        },
+      ]}>
+      <SearchBar
+        placeholder="Search for news"
+        onChangeText={searchNews}
+        value={keyword}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+});
+
+export default SearchBox;
